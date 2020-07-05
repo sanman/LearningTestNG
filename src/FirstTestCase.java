4cccfafa8f06140543b0f93ff13167d84f1e461c
sanjiv <learnsanjeev@gmail.com>
2020-07-05 01:05:33 -0400

    initial commit added first testcase for textng

---
+++ src/FirstTestCase.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class FirstTestCase {
+
+    @Test(priority=1)
+    void setup()
+    {
+        System.out.println("Opening Browser");
+    }
+
+    @Test(priority=2)
+    void login()
+    {
+        System.out.println("This is login test");
+    }
+
+    @Test(priority=3)
+    void teardown()
+    {
+        System.out.println("Closing Browser");
+    }
+}
